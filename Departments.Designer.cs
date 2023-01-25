e15c621e46ae7ee21e7266a74a70613132d2e3b8
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 17:37:06 +0200

    Changing its image to refer to logging out

--- Departments.Designer.cs
+++ Departments.Designer.cs
@@ -313,6 +313,7 @@
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 58;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
             // 
             // Departments
             // 
--- Departments.cs
+++ Departments.cs
@@ -26,5 +26,10 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
