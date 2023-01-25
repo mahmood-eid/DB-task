b772c5e4e0023960f2501095abffef0b831bbdd7
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 10:44:09 +0200

    changing its font color

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -44,6 +44,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(364, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
