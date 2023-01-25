990bdb61f411c5cf4a6143d19d044d901a4fd64d
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 13:54:30 +0200

    to loging in

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -112,11 +112,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 288);
+            this.button1.Location = new System.Drawing.Point(459, 323);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(137, 23);
             this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
+            this.button1.Text = "LOGIN";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Login
