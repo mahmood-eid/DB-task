bb5faf2bf81776be06cfc57462bc219ec43c47c0
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 20:23:48 +0200

    now everything is okay, let's move to the section of creating database

--- Salary.Designer.cs
+++ Salary.Designer.cs
@@ -190,9 +190,9 @@
             this.label2.ForeColor = System.Drawing.Color.Teal;
             this.label2.Location = new System.Drawing.Point(330, 152);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.Size = new System.Drawing.Size(128, 20);
             this.label2.TabIndex = 32;
-            this.label2.Text = "Manage Employees";
+            this.label2.Text = "Manage Salary";
             // 
             // label1
             // 
