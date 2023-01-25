5c365c5e3afeafb7698575bbf9d78ab05b44f848
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 10:48:57 +0200

    changing its text to Employee MS

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -48,9 +48,9 @@
             this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(364, 61);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Size = new System.Drawing.Size(107, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Employee MS";
             // 
             // Login
             // 
