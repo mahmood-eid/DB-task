8b01aa5e601e16de5419ad3d5a3af5d832423cc2
Mahm0ud-Eid <114058268+Mahm0ud-Eid@users.noreply.github.com>
2023-01-25 14:17:36 +0200

    changing its color

--- Employees.Designer.cs
+++ Employees.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Teal;
             this.label2.Location = new System.Drawing.Point(262, 124);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(219, 20);
